c8f63af6da559e80b13308e6139ae23884d1d6b3
shubh791 <shubhampanghal140@gmail.com>
2026-02-05 23:54:33 +0530

    category

--- client/src/components/home/feed/PostCard.jsx
+++ client/src/components/home/feed/PostCard.jsx
@@ -186,6 +186,25 @@ export default function PostCard({ post }) {
         )}
 
       </div>
+      {/* CATEGORY */}
+{post.category && (
+  <div className="mb-2">
+    <span className="
+      inline-block
+      text-[11px]
+      px-2 py-0.5
+      rounded-full
+      bg-cyan-500/10
+      text-cyan-400
+      border border-cyan-500/20
+      font-medium
+      whitespace-nowrap
+    ">
+      #{post.category}
+    </span>
+  </div>
+)}
+
 
       {/* CAPTION */}
 
